f5e3cd4a18348168cec6136c8318d5e3660b5efe
Kyungmin Lee <sephiroce@snu.ac.kr>
2020-04-20 22:09:01 +0900

    digging it!

--- tf_toys/capsnet/blocks.py
+++ tf_toys/capsnet/blocks.py
@@ -1,86 +1,122 @@
 #-*- coding:utf-8 -*-
 import tensorflow as tf
 
-
 def squash(s, axis=-1, epsilon=1e-7):
-  # for better numerical stability
-  squared_norm = tf.math.reduce_sum(tf.square(s), axis=axis, keepdims=True)
+  squared_norm = tf.reduce_sum(tf.square(s), axis=axis, keepdims=True)
   safe_norm = tf.sqrt(squared_norm + epsilon)
   squash_factor = squared_norm / (1. + squared_norm)
   unit_vector = s / safe_norm
   return squash_factor * unit_vector
 
-def safe_norm(s, axis=-1, epsilon=1e-7, keep_dims=False, name=None):
-  with tf.name_scope(name):
-    squared_norm = tf.reduce_sum(tf.square(s), axis=axis, keepdims=keep_dims)
-    return tf.sqrt(squared_norm + epsilon)
+def safe_norm(s, axis=-1, epsilon=1e-7, keepdims=False):
+  squared_norm = tf.reduce_sum(tf.square(s), axis=axis, keepdims=keepdims)
+  return tf.sqrt(squared_norm + epsilon)
 
 class CapsuleNetwork(tf.keras.layers.Layer):
   def __init__(self):
     super(CapsuleNetwork, self).__init__()
-    self.capsuler = CapsulizationLayer()
-    self.reconstructor = ReconstructionLayer()
+    self.caps1_n = caps1_n = 32
+    self.caps2_n = caps2_n = 10
+    self.caps1_dim = caps1_dim = 8
+    self.caps2_dim = caps2_dim = 16
+    self.caps_n = caps_n = caps1_n * 6 * 6
+    ks = 9
+
+    self.conv1 = tf.keras.layers.Conv2D(filters=256,
+                                        kernel_size=[ks, ks], strides=[1, 1],
+                                        padding="valid", activation="relu")
+
+    self.conv2 = tf.keras.layers.Conv2D(filters=caps1_n * caps1_dim,
+                                        kernel_size=[ks, ks], strides=[2, 2],
+                                        padding="valid", activation="relu")
+    #init_sigma = 0.1
+    #self.wgt = tf.Variable(tf.random.normal([1, caps_n, caps2_n, caps2_dim,
+    #                                         caps1_dim], stddev=init_sigma),
+    #                       trainable=True, name="W")
+
+    #self.reconstructor = ReconstructionLayer()
+    init_sigma = 0.1
+    self.wgt = tf.Variable(tf.random.normal([1, caps_n, caps2_n, caps2_dim,
+                                             caps1_dim], stddev=init_sigma),
+                           trainable=True, name="W")
 
   def call(self, inputs, **kwargs):
     image, target = inputs
     caps1_n_maps = 32 # number of capsule maps
-    caps2_n = 10 # number of classes
-    n_caps1 = caps1_n_maps * 6 * 6
+    caps2_n_caps = 10 # number of classes
+    caps1_n_caps = caps1_n_maps * 6 * 6
+    caps1_n_dims = 8
     is_train = kwargs["training"]
+    self.caps1_n = caps1_n = 32
+    self.caps2_n = caps2_n = 10
+    self.caps1_dim = caps1_dim = 8
+    self.caps2_dim = caps2_dim = 16
+    self.caps_n = caps_n = caps1_n * 6 * 6
 
     batch_size = tf.shape(image)[0]
-    # [batch, pc width, pc height, n_maps * n_dims]
-    u_hat = self.capsuler(image)
+    conv1 = self.conv1(image)
+    conv2 = self.conv2(conv1)
+    caps1_raw = tf.reshape(conv2, [-1, caps1_n_caps, caps1_n_dims],
+                           name="caps1_raw")
+    caps1_output = squash(caps1_raw)
+
+
+    W_tiled = tf.tile(self.wgt, [batch_size, 1, 1, 1, 1], name="W_tiled")
+    caps1_output_expanded = tf.expand_dims(caps1_output, -1,
+                                           name="caps1_output_expanded")
+    caps1_output_tile = tf.expand_dims(caps1_output_expanded, 2,
+                                       name="caps1_output_tile")
+    caps1_output_tiled = tf.tile(caps1_output_tile, [1, 1, caps2_n_caps, 1, 1],
+                                 name="caps1_output_tiled")
+    u_hat = tf.matmul(W_tiled, caps1_output_tiled, name="u_hat")
 
     # ==========================================================================
     # Dynamic Routing
     # ==========================================================================
-    # Line2: b: initialization
-    raw_wgts = tf.zeros([batch_size, n_caps1, caps2_n, 1, 1], dtype=tf.float32,
-                        name="raw_wgts")
+    raw_weights = tf.zeros([batch_size, caps1_n_caps, caps2_n_caps, 1, 1],
+                           dtype=tf.float32, name="raw_weights")
 
-    # Round1
-    # Line4: for all capsule i in layer l: c_i <- softmax(b_i)
-    # softmax over caps2_n
-    c_1 = tf.nn.softmax(raw_wgts, axis=2, name="routing_wgts_1")
-    # Line5: for all capsule j in layer (l + 1): s_j <- sum_i c_ij * u_hat_j|i
-    each_s_1 = tf.multiply(c_1, u_hat)
-    s_1 = tf.reduce_sum(each_s_1, axis=1, keepdims=True,
-                        name="wgted_sum_1")
-    # Line6: for all capsule j in layer (l + 1): v_j <- squash(s_j)
-    v_1 = squash(s_1, axis=-2)
-    # Line 7: for all capsule i in layer l and capsule j in layer (l+1):
-    #         b_ij <- b_ij + u_hat_j_i x v_j
-    v_1_tiled = tf.tile(v_1, [1, n_caps1, 1, 1, 1], name="round_1_out_tiled")
-    uv_1 = tf.matmul(u_hat, v_1_tiled, transpose_a=True, name="agreement")
-    raw_wgts2 = raw_wgts + uv_1
+    # Round1, Line 4
+    routing_weights = tf.nn.softmax(raw_weights, axis=2, name="routing_weights")
+    # Round1, Line 5
+    weighted_predictions = tf.multiply(routing_weights, u_hat,
+                                       name="weighted_predictions")
+    weighted_sum = tf.reduce_sum(weighted_predictions, axis=1, keepdims=True,
+                                 name="weighted_sum")
+    # Round1, Line 6
+    caps2_output_round_1 = squash(weighted_sum, axis=-2)
+    # Round1, Line 7
+    caps2_output_round_1_tiled = tf.tile(caps2_output_round_1,
+                                         [1, caps1_n_caps, 1, 1, 1],
+                                         name="caps2_output_round_1_tiled")
+    raw_weights2 = raw_weights + tf.matmul(u_hat, caps2_output_round_1_tiled,
+                                           transpose_a=True)
 
-    # Round2
-    # Line4: for all capsule i in layer l: c_i <- softmax(b_i)
-    c_2 = tf.nn.softmax(raw_wgts2, axis=2, name="routing_wgts_2")
-    # Line5: for all capsule j in layer (l + 1): s_j <- sum_i c_ij * u_hat_j|i
-    s_2 = tf.reduce_sum(tf.multiply(c_2, u_hat), axis=1, keepdims=True,
-                        name="wgted_sum_2")
-    # Line6: for all capsule j in layer (l + 1): v_j <- squash(s_j)
-    # tf.print("s_2 at round 2:", tf.shape(s_2))
-    v_2 = squash(s_2, axis=-2)
-    # Line7: won't be used the updated raw_wgts for next iteration
-    #===========================================================================
-
-    # [batch, 1, caps2_n, 1, 1]
-    norm_v2 = safe_norm(v_2, axis=-2, keep_dims=True, name="normed_round_2_out")
-    max_args = tf.argmax(norm_v2, axis=2)
-    # [batch, 1]
-    y_pred = tf.squeeze(max_args, axis=[1, 2], name="y_pred")
-
+    # Round2, Line 4
+    routing_weights_round_2 = tf.nn.softmax(raw_weights2, axis=2,
+                                            name="routing_weights_round_2")
+    # Round2, Line 5
+    weighted_predictions_round_2 = tf.multiply(routing_weights_round_2, u_hat,
+                                               name="weighted_predictions_round_2")
+    weighted_sum_round_2 = tf.reduce_sum(weighted_predictions_round_2, axis=1,
+                                         keepdims=True,
+                                         name="weighted_sum_round_2")
+    # Round2, Line 6
+    caps2_output = squash(weighted_sum_round_2, axis=-2)
+    # ==========================================================================
+    y_proba = tf.squeeze(safe_norm(caps2_output, axis=-2), 1)
+    #y_proba_argmax = tf.argmax(y_proba, axis=2, name="y_proba")
+    #y_pred = tf.squeeze(y_proba_argmax, axis=[1, 2], name="y_pred")
+    """
     # Reconstruction
     recon_tgts = tf.cast(target, tf.int32) if is_train else y_pred
     recon_mask = tf.reshape(tf.one_hot(recon_tgts, depth=caps2_n),
                             [-1, 1, caps2_n, 1, 1], name="recon_mask")
-    masked_v2 = tf.multiply(v_2, recon_mask, name="masked_round_2_out")
+    masked_v2 = tf.multiply(caps2_output, recon_mask, name="masked_round_2_out")
     dec_out = self.reconstructor(masked_v2)
-
-    return norm_v2, y_pred, dec_out
+    """
+    caps2_output_norm = safe_norm(caps2_output, axis=-2, keepdims=True)
+    return caps2_output_norm, y_proba#, dec_out
 
 
 class ReconstructionLayer(tf.keras.layers.Layer):
@@ -104,43 +140,3 @@ class ReconstructionLayer(tf.keras.layers.Layer):
     inputs = self.hidden1(inputs)
     inputs = self.hidden2(inputs)
     return self.decoder_output(inputs)
-
-
-class CapsulizationLayer(tf.keras.layers.Layer):
-  def __init__ (self):
-    super(CapsulizationLayer, self).__init__()
-    self.caps1_n = caps1_n = 32
-    self.caps2_n = caps2_n = 10
-    self.caps1_dim = caps1_dim = 8
-    self.caps2_dim = caps2_dim = 16
-    self.caps_n = caps_n = caps1_n * 6 * 6
-    ks = 9
-    init_sigma = 0.1
-
-    # caps1_n = 32 * caps1_wth * caps1_hgt, caps2_n = 10,
-    self.wgt = tf.Variable(tf.random.normal([1, caps_n, caps2_n, caps2_dim,
-                                             caps1_dim], stddev=init_sigma),
-                           trainable=True, name="W")
-    self.cnns = list()
-    self.cnns.append(tf.keras.layers.Conv2D(filters=256,
-                                            kernel_size=[ks, ks], strides=[1, 1],
-                                            padding="valid", activation="relu"))
-
-    self.cnns.append(tf.keras.layers.Conv2D(filters=caps1_n * caps1_dim,
-                                            kernel_size=[ks, ks], strides=[2, 2],
-                                            padding="valid", activation="relu"))
-
-  def call(self, inputs, **kwargs):
-    batch_size = tf.shape(inputs)[0]
-    for cnn in self.cnns:
-      inputs = cnn(inputs)
-
-    # [batch, pc width * pc height * n_caps, n_dims]: Primary Capsules
-    inputs = tf.reshape(inputs, [-1, self.caps_n, self.caps1_dim])
-    pc = squash(inputs)
-    pc_tiled = tf.tile(tf.expand_dims(tf.expand_dims(pc, -1), 2),
-                       [1, 1, self.caps2_n, 1, 1], name="caps1_output_tiled")
-    W_tiled = tf.tile(self.wgt, [batch_size, 1, 1, 1, 1], name="W_tiled")
-    result = tf.matmul(W_tiled, pc_tiled, name="digit_caps")
-
-    return result
--- tf_toys/capsnet/model.py
+++ tf_toys/capsnet/model.py
@@ -7,12 +7,48 @@ from tf_toys.capsnet.blocks import CapsuleNetwork
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-
 caps1_n_maps = 32
 caps1_n_dims = 8
 caps2_n = 10
 caps2_dim = 16
 n_caps1 = caps1_n_maps * 6 * 6
+
+
+@tf.function
+def train(inputs, loss_state, acc_state, model, optimizer, training=True):
+  image = tf.cast(inputs["image"], tf.float32)
+  targets = inputs["label"]
+
+  with tf.GradientTape() as tape:
+    norm_v2, y_pred = model([image, targets], training=training)
+
+    m_plus = 0.9
+    m_minus = 0.1
+    lambda_ = 0.5
+
+    # margin loss
+    T = tf.one_hot(targets, depth=caps2_n, name="T")
+    present_error_raw = tf.square(tf.maximum(0., m_plus - norm_v2),
+                                  name="present_error_raw")
+    present_error = tf.reshape(present_error_raw, shape=(-1, 10),
+                               name="present_error")
+    absent_error_raw = tf.square(tf.maximum(0., norm_v2 - m_minus),
+                                 name="absent_error_raw")
+    absent_error = tf.reshape(absent_error_raw, shape=(-1, 10),
+                              name="absent_error")
+    L = tf.add(T * present_error, lambda_ * (1.0 - T) * absent_error,
+               name="L")
+    loss = tf.reduce_mean(tf.reduce_sum(L, axis=1), name="margin_loss")
+
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+  loss_state.update_state(loss)
+  #correct = tf.equal(tf.cast(targets, tf.float32),
+  #                   tf.cast(y_pred, tf.float32), name="correct")
+  #accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name="accuracy")
+  acc_state.update_state(targets, y_pred)
+
 
 def main():
   #pylint: disable=too-many-locals
@@ -22,66 +58,21 @@ def main():
   mnist_builder = tfds.builder("mnist")
   mnist_builder.download_and_prepare()
   ds_train = mnist_builder.as_dataset(split="train")
-  ds_test = mnist_builder.as_dataset(split="test")
 
   ds_train = ds_train.repeat(1).shuffle(1024).batch(batch_size)
   ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
-  ds_test = ds_test.repeat(1).batch(batch_size)
-  ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
-
   caps_net = CapsuleNetwork()
-  optimizer = tf.keras.optimizers.Adam()
-  loss_object =\
-    tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+  opti = tf.keras.optimizers.Adam()
 
   n_iterations_per_epoch = 55000 // batch_size
-  @tf.function
-  def train(inputs, loss_state, acc_state, training=True):
-    image = tf.cast(inputs["image"], tf.float32)
-    targets = tf.cast(inputs["label"], tf.float32)
-
-    with tf.GradientTape() as tape:
-      norm_v2, y_pred, dec_out = caps_net([image, targets], training=training)
-
-      m_plus = 0.9
-      m_minus = 0.1
-      lambda_ = 0.5
-
-      # margin loss
-      present_err = tf.reshape(tf.square(tf.maximum(0., m_plus - norm_v2)),
-                               shape=(-1, 10), name="present_error")
-      absent_err = tf.reshape(tf.square(tf.maximum(0., norm_v2 - m_minus)),
-                              shape=(-1, 10), name="absent_error")
-      T = tf.one_hot(tf.cast(targets, tf.int32), depth=caps2_n, name="T")
-      margin_loss = T * present_err + lambda_ * (1.0 - T) * absent_err
-      margin_loss = tf.reduce_mean(tf.reduce_sum(margin_loss, axis=1),
-                                   name="margin_loss")
-
-      # reconstruction loss
-      flat_inp = tf.reshape(image, [-1, 28 * 28], name="X_flat")
-      recon_loss = tf.reduce_mean(tf.square(flat_inp - dec_out),
-                                  name="recon_loss")
-
-      alpha = 5e-4
-      #loss = margin_loss + alpha * recon_loss
-      loss = margin_loss
-      #tf.print(margin_loss, recon_loss)
-    grads = tape.gradient(loss, caps_net.trainable_variables)
-    optimizer.apply_gradients(zip(grads, caps_net.trainable_variables))
-
-    loss_state.update_state(loss)
-    y_pred = tf.squeeze(y_pred, axis=-1)
-    correct = tf.equal(tf.cast(targets, tf.float32),
-                       tf.cast(y_pred, tf.float32), name="correct")
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32), name="accuracy")
-    acc_state.update_state(accuracy)
 
   for epoch in range(1, 5):
-    epoch_loss_avg = tf.keras.metrics.Mean()
-    epoch_accuracy = tf.keras.metrics.Mean()
+    epoch_loss_avg = tf.keras.metrics.Mean(name='train_loss')
+    epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+      name='train_acc')
     for idx, datum in enumerate(iter(ds_train)):
-      train(datum, epoch_loss_avg, epoch_accuracy)
+      train(datum, epoch_loss_avg, epoch_accuracy, caps_net, opti)
       print('\rIteration: %d/%d loss %.3f accuracy %.3f' % (idx + 1,
                                                           n_iterations_per_epoch,
                                                           epoch_loss_avg.result(),
